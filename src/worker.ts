cdc6c1351de922782ae08d7e4e9ce965c6576451
Mike Weaver <michael@triplewhale.com>
2023-06-06 11:13:41 +0300

    eslint + mock

--- src/worker.ts
+++ src/worker.ts
@@ -1,15 +1,20 @@
-import { Worker } from '@temporalio/worker';
+import { Worker } from "@temporalio/worker";
 
 async function run() {
   const worker = await Worker.create({
+<<<<<<< Updated upstream
     workflowsPath: require.resolve('./workflow'),
     taskQueue: 'tutorial-workflow',
+=======
+    workflowsPath: require.resolve("./workflows"),
+    taskQueue: "tutorial-workflow",
+>>>>>>> Stashed changes
   });
   await worker.run();
   return worker;
 }
 
 run().catch((err) => {
-  console.error('Oh no!', err);
+  console.error("Oh no!", err);
   process.exit(1);
 });
