43fadb5f452afb18f137f4fa09ea553dea13f882
Mike Weaver <michael@triplewhale.com>
2023-06-06 11:14:17 +0300

    merge fix

--- src/worker.ts
+++ src/worker.ts
@@ -2,13 +2,8 @@ import { Worker } from "@temporalio/worker";
 
 async function run() {
   const worker = await Worker.create({
-<<<<<<< Updated upstream
-    workflowsPath: require.resolve('./workflow'),
-    taskQueue: 'tutorial-workflow',
-=======
     workflowsPath: require.resolve("./workflows"),
     taskQueue: "tutorial-workflow",
->>>>>>> Stashed changes
   });
   await worker.run();
   return worker;
