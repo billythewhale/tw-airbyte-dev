439838944dcabf9325e3d24e4787e428a76def97
Billy Duraney <billy@triplewhale.com>
2023-06-07 10:58:36 +0300

    also kill temporal server if there was an exit code

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-temporal server start-dev > /dev/null & TEMPORAL_SERVER_PID=$! && \
-  npx ts-node ./src/develop.ts && \
-  kill -INT $TEMPORAL_SERVER_PID
+temporal server start-dev > /dev/null & TEMPORAL_SERVER_PID=$!
+npx ts-node ./src/develop.ts
+kill -INT $TEMPORAL_SERVER_PID
